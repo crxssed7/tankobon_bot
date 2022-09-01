f78d16929722adcf6fe32ff755f7fadfe81b93a8
Tanveer Najib <tanveernajib@gmail.com>
2022-09-01 11:17:18 +0100

    update base url

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 ERROR_MSG = 'There was an error executing this command: @e'
 COMMAND_PREFIX = '$'
-API_BASEURL = 'https://tankobon-manga.herokuapp.com/api/'
+API_BASEURL = 'https://tankobon.fly.dev/api/'
